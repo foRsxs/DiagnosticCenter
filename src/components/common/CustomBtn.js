36bca7dab8e1d8bded012e2e85f3203139188695
Volodymyr <vmot1992@gmail.com>
2018-10-25 13:53:53 +0300

    add styles for customBtn, and fontSize in variables

---
+++ src/components/common/CustomBtn.js
@@ -0,0 +1,36 @@
+import React, {Component} from 'react';
+import {
+  Alert,
+  StyleSheet,
+  View,
+} from 'react-native';
+import {Text, Button} from 'native-base';
+import variables from '../../styles/variables'
+
+export default class CustomBtn extends Component {
+  render() {
+    return (
+      <Button onPress={() => this.props.onClick()} style={styles.btn}>
+        <Text style={styles.textBtn}>{this.props.label}</Text>
+      </Button>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  btn:{
+    alignSelf: 'center', 
+    justifyContent: 'center', 
+    width: '100%', 
+    height: 50, 
+    borderRadius: 25, 
+    backgroundColor: variables.colors.green
+  },
+  textBtn:{
+    fontFamily:'HelveticaNeue', 
+    fontSize: variables.fSize.main, 
+    lineHeight: 23, 
+    textAlign: 'center',
+    letterSpacing: 2
+  }
+});
--- src/styles/variables.js
+++ src/styles/variables.js
@@ -1,9 +1,30 @@
-const colors = {
-    blue: '#4e9eff',
-    white: '#ffffff',
-    lightBlue: 'rgba(78, 158, 255, 0.1)',
-    darkBlue: '#1C6693',
-    green: '#13CE67',
-    wiolet: 'rgba(130, 182, 255, 0.75)'
+const variables = {
+    colors: {
+        blue: '#4e9eff',
+        darkBlue: '#1C6693',
+        white: '#ffffff',
+        black: '#000000',
+        lightBlack: '#2A425F',
+        mediumBlack: '#24253D',
+        lightGray: 'rgba(78, 158, 255, 0.1)',
+        gray: 'rgba(112, 172, 245, 0.15)',
+        activeGray: 'rgba(112, 172, 245, 0.5)',
+        darkGray: 'rgba(42, 66, 95, 0.75)',
+        green: '#13CE67',
+        lightWiolet: 'rgba(130, 182, 255, 0.75)',
+        wiolet: '#82B6FF'
+    },
+    fonts: {
+        mainFont: 'HelveticaNeue'
+    },
+    fSize: {
+        main: 20,
+        medium: 25,
+        little: 15,
+        small: 18,
+        normal: 19,
+        big: 40,
+        large: 50
+    } 
 }
-export default colors;
+export default variables
