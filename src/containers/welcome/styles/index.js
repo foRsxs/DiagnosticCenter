9f869a66a688f3a6440bb6140cfc7bbe359c608b
Bogdan Balias <bodanbalias19@gmail.com>
2019-02-19 12:03:20 +0200

    done welcome screen

--- src/containers/welcome/styles/index.js
+++ src/containers/welcome/styles/index.js
@@ -2,48 +2,40 @@ import { StyleSheet } from 'react-native';
 
 import variables from '../../../styles/variables';
 const {large, medium} = variables.fSize;
-import { COLOR_BLUE, ACCENT_BLUE, COLOR_DARK_BLUE } from '../../../styles/constants';
+import { COLOR_BLUE, ACCENT_BLUE, COLOR_DARK_BLUE, MAIN_FONT } from '../../../styles/constants';
 
 export default StyleSheet.create({
 	wrapContainer: {
         justifyContent: 'space-between', 
         flexDirection: 'column',
-        paddingHorizontal: 40,
+        paddingHorizontal: 50,
     },
     title: {
         fontSize: large,
         color: COLOR_BLUE,
         textAlign: 'center',
-        marginTop: 40
+        marginTop: 40,
+        fontFamily: MAIN_FONT,
     },
     blueText: {
         fontSize: medium,
         color: ACCENT_BLUE,
         marginVertical: 15,
-        lineHeight: 17
+        lineHeight: 17,
+        fontFamily: MAIN_FONT,
     },
     darkText: {
+        fontFamily: MAIN_FONT,
         fontSize: medium,
         color: COLOR_DARK_BLUE,
-        lineHeight: 17
+        lineHeight: 20
     },
     textBold: {
+        fontFamily: MAIN_FONT,
         fontSize: medium,
         color: COLOR_DARK_BLUE,
         fontWeight: '600',
-        lineHeight: 17
-    },
-    image1: {
-        width: '100%',
-        height: 160
-    },
-    image2: {
-        width: '100%',
-        height: 300
-    },
-    image3: {
-        width: '100%',
-        height: 320
+        lineHeight: 20
     },
     button: {
         marginBottom: 40
