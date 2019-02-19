2ca714f7b9d19ec5ceb8789dcf5b1548ba04e269
Michael <m.polazhynets@hotmail.com>
2019-02-19 13:56:47 +0200

    Added RN offline && config persist

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,16 +1,60 @@
 import { createStore } from 'redux';
-import { persistStore, persistReducer } from 'redux-persist';
+import { persistStore, persistReducer, createTransform  } from 'redux-persist';
+import { offlineActionTypes, checkInternetConnection } from 'react-native-offline';
 import storage from 'redux-persist/lib/storage';
 
+import { authActions } from '../actions/auth';
+import { contentActions } from '../actions/content';
 import middleware from '../middleware';
 import rootReducer from '../reducers';
-import initialState from './initialState';
+
+const actions = {
+  authActions,
+  contentActions
+};
+
+const networkTransform = createTransform(
+  (inboundState, key) => {
+    const actionQueue = [];
+
+    inboundState.actionQueue.forEach(action => {
+      if (typeof action === 'function') {
+        actionQueue.push({
+          function: action.meta.name,
+          args: action.meta.args,
+        });
+      } else if (typeof action === 'object') {
+        actionQueue.push(action);
+      }
+    });
+
+    return {
+      ...inboundState,
+      actionQueue,
+    };
+  },
+  (outboundState, key) => {
+    const actionQueue = [];
+
+    outboundState.actionQueue.forEach(action => {
+      if (action.function) {
+        const actionFunction = actions[action.function];
+        actionQueue.push(actionFunction(...action.args));
+      } else {
+        actionQueue.push(action);
+      }
+    });
+    return { ...outboundState, actionQueue };
+  },
+  { whitelist: ['network'] },
+);
 
 const persistConfig = {
   timeout: 10000,
   key: 'root',
   storage,
-  whitelist: ['authorization'],
+  transforms: [networkTransform],
+  whitelist: ['authorization', 'content']
 }
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
@@ -18,12 +62,17 @@ const persistedReducer = persistReducer(persistConfig, rootReducer);
 export default function configureStore(callback) {
   let store = createStore(
     persistedReducer,
-    initialState,
     middleware
   );
 
   persistStore(store, null, () => {
-    callback();
+    checkInternetConnection().then(isConnected => {
+      store.dispatch({
+        type: offlineActionTypes.CONNECTION_CHANGE,
+        payload: isConnected,
+      });
+      callback();
+    });
   });
 
   if (module.hot) {
