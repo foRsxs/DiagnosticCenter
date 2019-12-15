cc91f3855598783d674023ad140929e84b1fbe91
MichaelPolazhynets <m.polazhynets@hotmail.com>
2019-12-15 23:38:40 +0200

    Mini changes

--- _docs/README.md
+++ _docs/README.md
@@ -23,11 +23,8 @@ mkdir -p android/app/src/main/assets && rm -rf android/app/build && npx react-na
 mkdir -p android/app/src/main/assets && rm -rf android/app/build && npx react-native bundle --platform android --dev false --entry-file index.js --bundle-output android/app/src/main/assets/index.android.bundle --assets-dest android/app/src/main/res && cd android && ./gradlew assembleRelease && cd ../
 
 If ERROR when run dev-mode, need clean all build android:
-cd android && ./gradlew clean
+cd android && ./gradlew clean && cd ../
 
 Run: react-native run-android --variant=release
-
-Создание билда для ios
-react-native bundle --entry-file index.js --platform ios --dev false --bundle-output ios/main.jsbundle --assets-dest ios
 
 react-native run-ios --configuration Release
--- android/app/src/main/java/com/izzisoftware/diagnosticcenter/MainApplication.java
+++ android/app/src/main/java/com/izzisoftware/diagnosticcenter/MainApplication.java
@@ -1,12 +1,10 @@
 package com.izzisoftware.diagnosticcenter;
 
 import android.app.Application;
-
 import com.facebook.react.ReactApplication;
 import com.learnium.RNDeviceInfo.RNDeviceInfo;
 import org.reactnative.camera.RNCameraPackage;
 import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
-import com.reactnativecommunity.netinfo.NetInfoPackage;
 import com.reactnativecommunity.webview.RNCWebViewPackage;
 import com.BV.LinearGradient.LinearGradientPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
@@ -14,7 +12,6 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
-
 import com.RNFetchBlob.RNFetchBlobPackage;
 import com.geektime.rnonesignalandroid.ReactNativeOneSignalPackage;
 import com.rnfingerprint.FingerprintAuthPackage;
@@ -41,7 +38,6 @@ public class MainApplication extends Application implements ShareApplication, Re
         new RNDeviceInfo(),
         new RNCameraPackage(),
         new AsyncStoragePackage(),
-        new NetInfoPackage(),
         new RNCWebViewPackage(),
         new LinearGradientPackage(),
         new RNGestureHandlerPackage(),
