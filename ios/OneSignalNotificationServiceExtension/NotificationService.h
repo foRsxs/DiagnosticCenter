0300624b6376e0eafb928d1a7530361fd8cdaee3
Michael P <m.polazhynets@hotmail.com>
2020-10-07 16:04:25 +0300

    Fix for android && added OneSignal Notification Extension

---
+++ ios/OneSignalNotificationServiceExtension/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  OneSignalNotificationServiceExtension
+//
+//  Created by iMac on 07.10.2020.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
